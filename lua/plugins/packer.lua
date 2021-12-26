9ab42f051d54509541445ce450f453263e8c44bb
Benoit Benedetti <humboldtux@gmail.com>
2021-12-26 15:19:26 +0100

    reorder plugins

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -16,6 +16,9 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 -- Use a protected call so we don't error out on first use
+-- pcall == protected call
+-- au lieu de
+---- local packer = require("packer")
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
   return
@@ -46,66 +49,23 @@ return packer.startup(function(use)
   use 'kyazdani42/nvim-web-devicons'
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
 
-  use {
-    'kyazdani42/nvim-tree.lua',
-    config = function() require'nvim-tree'.setup {} end
-  }
-
-  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
-  use "numToStr/Comment.nvim" -- Easily comment stuff
-  use "akinsho/bufferline.nvim"
-  use "moll/vim-bbye"
-  use 'nvim-lualine/lualine.nvim'
-  use "akinsho/toggleterm.nvim"
-
-  --use 'ggandor/lightspeed.nvim'
-  --use 'unblevable/quick-scope'
-  use 'lewis6991/impatient.nvim'
-
-  -- Dashboard
-  --use 'mhinz/vim-startify'
-  --use 'glepnir/dashboard-nvim'
-  use 'goolord/alpha-nvim'
-  use "ahmedkhalf/project.nvim"
-
-  use "folke/which-key.nvim"
-
   -- Themes
   -- use 'shaunsingh/nord.nvim'
   -- use 'joshdick/onedark.vim'
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
+  use 'nvim-lualine/lualine.nvim'
 
-  -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp"
-  use 'onsails/lspkind-nvim'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
-
-  use 'p00f/nvim-ts-rainbow'
-  use 'norcalli/nvim-colorizer.lua'
-  use 'simrat39/rust-tools.nvim'
-
-  -- snippets
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
-  -- use 'hrsh7th/cmp-vsnip'
-  -- use 'hrsh7th/vim-vsnip'
-  -- use 'saadparwaiz1/cmp_luasnip'
-
-  -- LSP
-  use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
-  use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
-  use "jose-elias-alvarez/null-ls.nvim"
+  use {
+    'kyazdani42/nvim-tree.lua',
+    config = function() require'nvim-tree'.setup {} end
+  }
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
+  use 'nvim-telescope/telescope-media-files.nvim'
+
+  use "akinsho/bufferline.nvim"
 
   -- Treesitter
   -- Highlight, edit, and navigate code using a fast incremental parsing library
@@ -113,19 +73,52 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
-
+  use 'p00f/nvim-ts-rainbow' -- treesitter rainbow plugin
   use 'JoosepAlviste/nvim-ts-context-commentstring'
+
+  -- Completion
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "hrsh7th/cmp-nvim-lua" -- cmdline completions
+  use 'onsails/lspkind-nvim'
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+  use "jose-elias-alvarez/null-ls.nvim"
 
   use 'tpope/vim-fugitive' -- Git commands in nvim
   use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
   use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
   use "lewis6991/gitsigns.nvim"
 
-  use 'sbdchd/neoformat'
-  use 'lukas-reineke/indent-blankline.nvim'
-  use 'ellisonleao/glow.nvim'
+  -- Dashboard
+  --use 'mhinz/vim-startify'
+  --use 'glepnir/dashboard-nvim'
+  use 'goolord/alpha-nvim'
+  use "ahmedkhalf/project.nvim"
 
-  use 'NTBBloodbath/rest.nvim'
+  use "akinsho/toggleterm.nvim"
+  use "folke/which-key.nvim"
+
+  --use 'ggandor/lightspeed.nvim'
+  --use 'unblevable/quick-scope'
+  use 'lewis6991/impatient.nvim'
+
+  ---- MORE
+  -----------
+  use 'simrat39/rust-tools.nvim'
 
   use {
     "AckslD/nvim-neoclip.lua",
@@ -133,6 +126,17 @@ return packer.startup(function(use)
       require("neoclip").setup()
     end,
   }
+
+  use 'sbdchd/neoformat'
+  use 'lukas-reineke/indent-blankline.nvim'
+  use 'ellisonleao/glow.nvim'
+
+  use 'NTBBloodbath/rest.nvim'
+
+  use 'norcalli/nvim-colorizer.lua'
+  use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
+  use "numToStr/Comment.nvim" -- Easily comment stuff
+  use "moll/vim-bbye"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
@@ -151,6 +155,7 @@ end)
 --'junegunn/goyo.vim' "distraction free writing
 --'junegunn/limelight.vim' "distraction, free writing with goyo
 --'vimwiki/vimwiki', { 'branch': 'dev' }
+-- https://github.com/sjl/gundo.vim
 
 --Terminal
 --https://github.com/akinsho/toggleterm.nvim
