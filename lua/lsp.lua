baa1d6ab8d8ccc8a7eb922fd572c655f2608f19d
Benoit Benedetti <humboldtux@gmail.com>
2022-01-01 20:41:46 +0100

    Fix lsp for rust/markdown/tailwind

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -3,56 +3,112 @@ if not status_ok then
 	return
 end
 
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+	return
+end
+
 local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
 
 for type, icon in pairs(signs) do
-  local hl = "LspDiagnosticsSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "LspDiagnosticsSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 --capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
-  return
+	return
 end
 capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 --local nvim_lsp = require'lspconfig'
 
-local servers = { 'clangd', 'rust_analyzer', 'bashls', 'yamlls', 'vimls', 'cssls', 'html', 'jsonls', 'taplo' }
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    capabilities = capabilities,
-  }
+local servers = {
+	"clangd",
+	"rust_analyzer",
+	"tailwindcss",
+	"bashls",
+	"yamlls",
+	"vimls",
+	"cssls",
+	"html",
+	"jsonls",
+	"sumneko_lua",
+}
+for _, name in pairs(servers) do
+	local server_is_found, server = lsp_installer.get_server(name)
+	if server_is_found then
+		if not server:is_installed() then
+			print("Installing " .. name)
+			server:install()
+		end
+	end
 end
 
-local runtime_path = vim.split(package.path, ';')
+for _, lsp in ipairs(servers) do
+	nvim_lsp[lsp].setup({
+		capabilities = capabilities,
+	})
+end
+
+local runtime_path = vim.split(package.path, ";")
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-require'lspconfig'.sumneko_lua.setup {
-  cmd = {"/usr/local/bin/lua-language-server"};
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path,
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
+require("lspconfig").taplo.setup({})
+
+require("lspconfig").sumneko_lua.setup({
+	--cmd = {"/usr/local/bin/lua-language-server"};
+	cmd = { "/home/benben/.local/share/nvim/lsp_servers/sumneko_lua/extension/server/bin/lua-language-server" },
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+				-- Setup your lua path
+				path = runtime_path,
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
+
+require("lspconfig").tailwindcss.setup({
+	--cmd = {"/home/benben/.local/share/nvim/lsp_servers/ltex/ltex-ls"};
+	cmd = {
+		"/home/benben/.local/share/nvim/lsp_servers/tailwindcss_npm/node_modules/@tailwindcss/language-server/bin/tailwindcss-language-server",
+	},
+})
+
+local lsp_installer = require("nvim-lsp-installer")
+
+lsp_installer.on_server_ready(function(server)
+	local opts = {}
+
+	if server.name == "rust_analyzer" then
+		-- Initialize the LSP via rust-tools instead
+		require("rust-tools").setup({
+			-- The "server" property provided in rust-tools setup function are the
+			-- settings rust-tools will provide to lspconfig during init.            --
+			-- We merge the necessary settings from nvim-lsp-installer (server:get_default_options())
+			-- with the user's own settings (opts).
+			server = vim.tbl_deep_extend("force", server:get_default_options(), opts),
+		})
+		server:attach_buffers()
+	else
+		server:setup(opts)
+	end
+end)
