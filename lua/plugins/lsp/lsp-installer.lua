469d26fa925882465ca645f7c2c2521a48ca8dd1
Benoit Benedetti <humboldtux@gmail.com>
2022-01-12 22:30:55 +0100

    auto install lsp

--- lua/plugins/lsp/lsp-installer.lua
+++ lua/plugins/lsp/lsp-installer.lua
@@ -3,6 +3,28 @@ if not status_ok then
 	return
 end
 
+local servers = {
+	"clangd",
+	"rust_analyzer",
+	"tailwindcss",
+	"bashls",
+	"yamlls",
+	"vimls",
+	"cssls",
+	"html",
+	"jsonls",
+	"sumneko_lua",
+}
+for _, name in pairs(servers) do
+	local server_is_found, server = lsp_installer.get_server(name)
+	if server_is_found then
+		if not server:is_installed() then
+			print("Installing " .. name)
+			server:install()
+		end
+	end
+end
+
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
