69d586c700e5b385b0683b5c8cc93f5e0b40493d
Benoit Benedetti <humboldtux@gmail.com>
2022-01-13 15:38:48 +0100

    conf sumneko_lua: cmd path

--- lua/plugins/lsp/settings/sumneko_lua.lua
+++ lua/plugins/lsp/settings/sumneko_lua.lua
@@ -1,8 +1,9 @@
 return {
+	cmd = { vim.fn.stdpath("data") .. "/lsp_servers/sumneko_lua/extension/server/bin/lua-language-server" },
 	settings = {
-
 		Lua = {
 			diagnostics = {
+				-- Get the language server to recognize the `vim` global
 				globals = { "vim" },
 			},
 			workspace = {
