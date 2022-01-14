f9d07c66178de384b60b489e6de0ff4c2492324a
Benoit Benedetti <humboldtux@gmail.com>
2022-01-14 17:52:27 +0100

    lsp: taplo conf

--- lua/plugins/lsp/lsp-installer.lua
+++ lua/plugins/lsp/lsp-installer.lua
@@ -53,6 +53,10 @@ lsp_installer.on_server_ready(function(server)
 		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
 	end
 
+	if server.name == "taplo" then
+		local taplo_opts = require("plugins.lsp.settings.taplo")
+		opts = vim.tbl_deep_extend("force", taplo_opts, opts)
+	end
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
