cd9c6746b8f1c13e14db436ad70d78c412d5d15b
Benoit Benedetti <humboldtux@gmail.com>
2022-01-24 15:45:22 +0100

    ménage toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,46 +4,12 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	size = 20,
 	open_mapping = [[<C-t>]],
-	hide_numbers = true,
-	shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = 2,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
 	direction = "float",
-	close_on_exit = true,
-	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
-		winblend = 0,
-		highlights = {
-			border = "Normal",
-			background = "Normal",
-		},
-	},
 })
 
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
 function _LAZYGIT_TOGGLE()
 	lazygit:toggle()
 end
-
-local status_ok, which_key = pcall(require, "which-key")
-if not status_ok then
-	return
-end
-
-which_key.register({
-	t = {
-		name = "Terminal",
-		g = { "<cmd>lua _LAZYGIT_TOGGLE()<cr>", "LazyGit" },
-		f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-		v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
-	},
-}, { prefix = "<leader>" })
