0a726ce32403654baea88b2094cefb3920e15326
Benoit Benedetti <humboldtux@gmail.com>
2022-01-24 11:19:35 +0100

    Ménage treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -47,15 +47,15 @@ configs.setup({
 	},
 })
 
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.nushell = {
-	install_info = {
-		url = "~/dev/src/github.com/humboldtux/tree-sitter-nushell", -- local path or git repo
-		files = { "src/parser.c" },
-		-- optional entries:
-		branch = "main", -- default branch in case of git repo if different from master
-		generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-		requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-	},
-	filetype = "nu", -- if filetype does not agrees with parser name
-}
+--local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+--parser_config.nushell = {
+--	install_info = {
+--		url = "~/dev/src/github.com/humboldtux/tree-sitter-nushell", -- local path or git repo
+--		files = { "src/parser.c" },
+--		-- optional entries:
+--		branch = "main", -- default branch in case of git repo if different from master
+--		generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+--		requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+--	},
+--	filetype = "nu", -- if filetype does not agrees with parser name
+--}
